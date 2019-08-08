97bdd7e137083b7bce2c89760c1124ff42518baf
“chenyixing” <1663268062@qq.com>
2019-08-08 08:57:25 +0800

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,59 @@
 import java.util.*;
 
+import com.swust.二叉树.BST;
 import com.swust.数组.Array;
 
 
 public class Main {
 	public static void main(String[] args){
 		
-		List<Integer> list = new ArrayList<>();
+		StringBuilder sb1 = new StringBuilder();
+		StringBuilder sb2 = new StringBuilder();
+
+		BST<Integer> bst1 = new BST<>();
+		BST<Integer> bst2 = new BST<>();
+		bst1.add(8);bst1.add(8);bst1.add(7);
+		
+		List<String> list = new ArrayList<>();
+		list.add("chen");
 		
 //		Set<Integer> set = new HashSet<>();
 		
 		
-		TreeMap<String,String> map = new TreeMap<>();
+//		TreeMap<String,String> map = new TreeMap<>();
+//		
+//		TreeSet<Integer> set = new TreeSet<>();
+//		set.add(4);set.add(5);set.add(1);set.add(6);set.add(2);set.add(7);set.add(3);set.add(8);
+//		sop(set.toString());
+//		
+//		int[] input = null;
+//		sop(input.length);
 		
-		TreeSet<Integer> set = new TreeSet<>();
-		set.add(4);set.add(5);set.add(1);set.add(6);set.add(2);set.add(7);set.add(3);set.add(8);
-		sop(set.toString());
-		
-		int[] input = null;
-		sop(input.length);
 	
  	}
+	
+	public class TreeNode {
+	    int val = 0;
+	    TreeNode left = null;
+	    TreeNode right = null;
+
+	    public TreeNode(int val) {
+	        this.val = val;
+
+	    }
+	}
+	
+	
+	
+	/**
+		输入一颗二叉树的根节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
+		路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前)
+	*/
+	
+	public static void run(StringBuilder sb){
+		sb.append("hh");
+		
+	}
 	
 	public static void sop(Object obj){
 		System.out.println(obj);
