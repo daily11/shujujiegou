bda897bca3f2f951514f740f82f0424f53a8dd39
“chenyixing” <1663268062@qq.com>
2019-08-15 16:30:35 +0800

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.LinkedList;
 
 import com.swust.二叉树.BST;
 import com.swust.数组.Array;
@@ -6,48 +6,8 @@ import com.swust.数组.Array;
 
 public class Main {
 	public static void main(String[] args){
-		
-		StringBuilder sb1 = new StringBuilder();
-		StringBuilder sb2 = new StringBuilder();
-
-		BST<Integer> bst1 = new BST<>();
-		BST<Integer> bst2 = new BST<>();
-		bst1.add(8);bst1.add(8);bst1.add(7);
-		
-		List<String> list = new ArrayList<>();
-		list.add("chen");
-		
-//		Set<Integer> set = new HashSet<>();
-		
-<<<<<<< HEAD
-		StringBuilder sb = new StringBuilder();
-		sb.append(1+"!");
-		sb.append("#!");
-		sb.append(2);
-		sop(sb.toString());
-		
-=======
-	
->>>>>>> 1c9062f8a3c509ee257569031fafc09f77225bdd
+		LinkedList<Integer> list = new LinkedList<>();
  	}
-	
-	public class TreeNode {
-	    int val = 0;
-	    TreeNode left = null;
-	    TreeNode right = null;
-
-	    public TreeNode(int val) {
-	        this.val = val;
-
-	    }
-	}
-	
-	
-	
-	/**
-		输入一颗二叉树的根节点和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
-		路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。(注意: 在返回值的list中，数组长度大的数组靠前)
-	*/
 	
 	public static void run(StringBuilder sb){
 		sb.append("hh");
--- src/com/swust/队列/Queue.java
+++ src/com/swust/队列/Queue.java
@@ -1,6 +1,38 @@
 package com.swust.队列;
 
 public interface Queue<E> {
+	
+//    private class Node{
+//        public E e;
+//        public Node next;
+//
+//        public Node(E e, Node next){
+//            this.e = e;
+//            this.next = next;
+//        }
+//
+//        public Node(E e){
+//            this(e, null);
+//        }
+//
+//        public Node(){
+//            this(null, null);
+//        }
+//
+//        @Override
+//        public String toString(){
+//            return e.toString();
+//        }
+//    }
+//
+//    private Node head, tail;
+//    private int size;
+//
+//    public LinkedListQueue(){
+//        head = null;
+//        tail = null;
+//        size = 0;
+//    }
 
     void offer(E e);
     E poll();
