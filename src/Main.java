657704aac65c11fd13a5a11ca19ae568a35024b5
“chenyixing” <1663268062@qq.com>
2019-08-26 16:15:28 +0800

    add code

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,40 @@
 import java.util.*;
 
 import com.suanfa.排序.*;
-import com.suanfa.查找.HaXi;
 
 
 public class Main {
+	static int[] data = new int[]{3,4,2,6};
 	
 	public static void main(String[] args){
-		int[] arr = new int[]{3,4,2,6};
+		
  	}
 	
+	public static int query(int value){
+
+		return -1;
+	}
 	
+	public static void sort(){
+		
+	}
+	
+	private static void swap(int i, int j) {
+		int temp = data[j];
+		data[j] = data[i];
+		data[i] = temp;
+	}
+	
+	private static void print(){
+		for(int i=0;i<data.length;i++){
+			if(i!=data.length-1){
+				System.out.print(data[i]+",");
+			}else{
+				System.out.print(data[i]);
+			}
+		}
+	}
+
 	public static void sop(Object obj){
 		System.out.println(obj);
 	}
